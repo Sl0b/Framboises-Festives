4ba75e81a3a4a2a6f6a83ffc31977148f9295336
balatzar <bataprod@gmail.com>
2015-11-19 18:55:20 +0100

    basic styling menu bar

--- client/templates/app-body.js
+++ client/templates/app-body.js
@@ -1,3 +1,8 @@
-Template.appBody.helpers({
-  
+Template.appBody.events({
+  "click .blur": function() {
+    var menu = document.querySelector(".menu");
+    var blur = document.querySelector(".blur");
+    menu.classList.toggle("hidden");
+    blur.classList.toggle("hidden");
+  }
 })
--- client/templates/nav.js
+++ client/templates/nav.js
@@ -1,6 +1,8 @@
 Template.nav.events({
   "click .nav": function() {
     var menu = document.querySelector(".menu");
+    var blur = document.querySelector(".blur");
     menu.classList.toggle("hidden");
+    blur.classList.toggle("hidden");
   }
 })
