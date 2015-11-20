44b45a40e8eaa00af9d2dc4b54e7a9bbe29aaf22
Laurent Berthier <proberthier@gmail.com>
2015-11-20 14:21:07 +0100

    added the possibility to pay, verify and validate payment

--- models/framboises.js
+++ models/framboises.js
@@ -5,5 +5,9 @@ Meteor.methods({
     var obj = {};
     obj[username] = payValue;
     Framboisiers.update(framboisierId, {$push: {holdingPayments: obj } })
+  },
+  validatePayment: function(framboisierId, obj) {
+    Framboisiers.update(framboisierId, {$pull: {holdingPayments: obj } })
+    Framboisiers.update(framboisierId, {$push: {closedPayments: obj } })
   }
 });
